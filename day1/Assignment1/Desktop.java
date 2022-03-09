7952577fa277f150f3b0a5c54b8841119a3e59a7
Ramakrishna-1989 <71293725+Ramakrishna-1989@users.noreply.github.com>
2022-03-09 16:58:52 +0530

    Add files via upload

---
+++ day1/Assignment1/Desktop.java
@@ -0,0 +1,16 @@
+package week3.day1.org.system;
+
+public class Desktop extends Computer {
+
+	public static void main(String[] args) {
+
+		Desktop dp = new Desktop();
+		dp.computerModel();
+		dp.desktopSize();
+	}
+
+	public void desktopSize() {
+		System.out.println("Desktop Size is 17 inches");
+	}
+	
+}
