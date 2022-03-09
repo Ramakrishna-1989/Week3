cc63fa59d244cd641742c16a9cea143e68b6264c
Ramakrishna-1989 <71293725+Ramakrishna-1989@users.noreply.github.com>
2022-03-09 17:06:27 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,20 @@
+package week3.day1;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("BankInfo Class : Printing the saving method" );
+	}
+
+	public void fixed() {
+		System.out.println("BankInfo Class : Printing the fixed method" );
+
+	}
+
+	public void deposit() {
+		System.out.println("BankInfo Class : Printing the deposit method" );
+
+	}
+	
+}
+
